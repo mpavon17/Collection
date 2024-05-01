4dffb667c7fd9f134d2c0291f9a4721e56c72a02
miguel <spavon17@gmail.com>
2024-05-01 19:57:37 +0200

    botones logOut e Inicio

--- app/src/main/java/com/example/collection/MainActivity.java
+++ app/src/main/java/com/example/collection/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,11 +25,13 @@ public class MainActivity extends AppCompatActivity {
     DatabaseReference bartReference;
 
     Button boton1;
+    ImageButton salir, home;
     EditText nombre1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void agregar(View v) {
@@ -36,6 +39,19 @@ public class MainActivity extends AppCompatActivity {
         Agregar a = new Agregar();
         FragmentTransaction ft = fm.beginTransaction();
         ft.replace(R.id.frameagregar, a);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    public void salir (View v) {
+        finish();
+    }
+
+    public void inicio(View v) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        Inicio i = new Inicio();
+        ft.replace(R.id.frameagregar,i);
         ft.addToBackStack(null);
         ft.commit();
     }
