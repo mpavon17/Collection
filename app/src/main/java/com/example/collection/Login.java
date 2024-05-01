25314d3b091f4c75e79cd44df1f6b7b29a5f44d2
miguel <spavon17@gmail.com>
2024-05-01 17:13:12 +0200

    Login

--- app/src/main/java/com/example/collection/Login.java
+++ app/src/main/java/com/example/collection/Login.java
@@ -1,25 +1,71 @@
 package com.example.collection;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login extends AppCompatActivity {
+
+    Button okey, registro;
+    EditText usuario, contrasena;
+
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-
+        okey = findViewById(R.id.ok);
+        registro = findViewById(R.id.registro);
+        usuario = findViewById(R.id.usuario);
+        contrasena = findViewById(R.id.password);
+        mAuth = FirebaseAuth.getInstance();
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.createUserWithEmailAndPassword(usuario.getText().toString(), contrasena.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Login.this, "Usuario creado", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(Login.this, "Error al crear usuario", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+        okey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signInWithEmailAndPassword(usuario.getText().toString(), contrasena.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(Login.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(Login.this, "Error al logear usuario", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
         });
     }
-}
+    }
